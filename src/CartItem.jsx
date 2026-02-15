9011d67de77046d139c038454dabeccf19f074f5
skoczny <kacper.skoneczny@o2.pl>
2026-02-15 18:41:09 -0500

    Complete development of the CarItem component and fix productlist component

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,42 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0
+    cart.forEach((item) => {
+        const quantity = item.quantity
+        const cost = parseFloat(item.cost.substring(1))
+        total += quantity * cost
+    })
+    return total
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e)
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1 }))
   };
 
   const handleDecrement = (item) => {
-   
+   if(item.quantity > 1){
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}))
+   }else{
+    dispatch(removeItem(item.name))
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const cost = parseFloat(item.cost.substring(1))
+    const total = item.quantity * cost
+    return total
   };
 
   return (
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -22,7 +22,7 @@ export const CartSlice = createSlice({
         const {name, quantity} = action.payload
         const itemToUpdate = state.items.find(item => item.name === name)
         if(itemToUpdate){
-            itemToUpdate.quantity = qua
+            itemToUpdate.quantity = quantity
         }
     
     },
